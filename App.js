08381a9ae52a914770936cb6e75458b65aff7324
Umesh Kumar <umeshsingla05@gmail.com>
2024-06-04 23:10:55 +0530

    Episode 3

--- App.js
+++ App.js
@@ -1,14 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement("h1", {}, "This is a nested heading level 1 inside a parent and child div.");
-const heading2 = React.createElement("h2", {}, "This is a nested heading level 2 inside a parent and child div.");
-const child = React.createElement("div", {id: "child"}, [heading1, heading2] );
-const parent = React.createElement(
-    "div", 
-    {id: "parent"}, 
-    child 
-);
+// JSX is transpiled before it reaches JS - Parcel - Babel
 
-const root = ReactDOM.createRoot(document.getElementById("container__root-react"));
-root.render(parent);
+// JSX => React.CreateElement => React Element - JS Object => HTML Element( React Render ).
+const heading = <h1 className="heading">Hello World, this is React using JSX.</h1>;
+const headingMultipleLine = ( <h1 className="heading">
+    Hello World, this is React using JSX.
+</h1> );
+
+const elem = <span>This is a span tag content.</span>;
+const Title = () => {
+    return <h1 id="heading">This is the heading coming from Title component. {elem}</h1>;
+}
+
+const num = 440;
+//Functional Component.
+const HeadingComponent = () => {
+    return (
+        <div className="container">
+            <Title />
+            <h2 className="heading">Hello World {num}, this is Heading level 2 using functional component - HeadingComponent.</h2>
+        </div>
+    );
+}
+const root = ReactDOM.createRoot(document.getElementById("container__root"));
+
+root.render( <HeadingComponent />);
